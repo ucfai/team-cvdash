c65ee6b75f757fd9a4146b0b27e9de820b54b19b
Calvin Yong <calvinpyong@gmail.com>
2019-10-20 16:39:10 -0400

    update app

--- app.py
+++ app.py
@@ -1,23 +1,31 @@
 #!/usr/bin/env python
-import datetime
 import base64
+import datetime
 from io import BytesIO
-from PIL import Image
-import numpy as np
+
 import dash
-from dash.dependencies import Input, Output, State
 import dash_core_components as dcc
 import dash_html_components as html
+import numpy as np
+from dash.dependencies import Input, Output, State
+from PIL import Image
+
 from cvdash.tasks import classification
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
+colors = {
+    'background': '343434'
+}
+
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-app.layout = html.Div([
-    html.Div(id='title',children=html.H1(children='CVDash'),
+app.layout = html.Div(
+    style={'backgroundColor': colors['background']},
+    children=[
+    html.Div(id='title', children=html.H1(children='CVDash'),
         style={
-            'textAlign':'center'
+            'textAlign': 'center'
         }),
     dcc.Upload(
         id='upload-image',
@@ -34,34 +42,27 @@ app.layout = html.Div([
             'borderStyle': 'dashed',
             'borderRadius': '5px',
             'textAlign': 'center',
-            'margin': '10px'
+            'margin': '10px',
         },
         # Allow multiple files to be uploaded
         multiple=True
-    ),  
+    ),
     html.Div(
         id='output-image-upload',
         style={
-            'width': '40%',
+            'width': '45%',
             'display': 'inline-block',
             'margin': '1em'
         }
     ),
     html.Div([dcc.Graph(
-            id='example-graph',
-            figure={
-                'data': [
-                    {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-                    {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
-                ],
-                'layout': {
-                    'title': 'Dash Data Visualization'
-                }
-            })
+            id='bar_graph',
+            figure=# inital graph
+            )
         ],
         style={
-            'width':'40%',
-            'display':'inline-block',
+            'width': '45%',
+            'display': 'inline-block',
             'float': 'right',
             'margin': '1em'
         }
@@ -71,38 +72,31 @@ app.layout = html.Div([
 
 def parse_contents(contents):
     return html.Div([
-        #html.H5(filename),
-        #html.H6(datetime.datetime.fromtimestamp(date)),
-
-        # HTML images accept base64 encoded strings in the same format
-        # that is supplied by the upload
-        html.Img(src=contents),
-        #html.Hr(),
-        #html.Div('Raw Content'),
-        #html.Pre(contents[0:200] + '...', style={
-        #    'whiteSpace': 'pre-wrap',
-        #    'wordBreak': 'break-all'
-        #})
+        html.Img(src=contents,
+            style={
+                'max-width': '100%',
+                'max-height': '100%',
+                'align': 'middle'
+            }),
     ])
 
 
-@app.callback(Output('output-image-upload', 'children'),
-              [Input('upload-image', 'contents')],
-              [State('upload-image', 'filename'),
-               State('upload-image', 'last_modified')])
+@app.callback([Output('output-image-upload', 'children'),
+            Output()],
+              [Input('upload-image', 'contents')])
 def update_output(list_of_contents, list_of_names, list_of_dates):
     if list_of_contents is not None:
-        #children = [
+        # children = [
         #    parse_contents(c, n, d) for c, n, d in
         #    zip(list_of_contents, list_of_names, list_of_dates)]
         children = [parse_contents(item) for item in list_of_contents]
-
 
         string_b64 = str(list_of_contents[0])[23:]
         image = b64_to_np(string_b64)
         classification.main(image, 5)
 
         return children
+
 
 def b64_to_np(string):
     x = base64.b64decode(string)
@@ -113,6 +107,17 @@ def b64_to_np(string):
     im = np.array(image, dtype=np.float32)
 
     return im
+
+
+def np_to_b64(arr):
+    return base64.b64encode(arr)
+
+
+def b64_to_PIL(string):
+    x = base64.b64decode(string)
+    decoded = BytesIO(x)
+    image = Image.open(decoded)
+    return image
 
 
 if __name__ == '__main__':
