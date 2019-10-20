ef3cb133e35d28bcc9fc4937f2a3d4bca04617d8
Calvin Yong <calvinpyong@gmail.com>
2019-10-20 16:31:37 -0400

    update classification

--- cvdash/tasks/classification.py
+++ cvdash/tasks/classification.py
@@ -1,18 +1,19 @@
+"""Functions for Classification tab
+"""
 from io import BytesIO
 
 import cv2
-import matplotlib.pyplot as plt
 import numpy as np
-import requests
-import plotly.offline as py
 import plotly.graph_objects as go
-
+import requests
 from PIL import Image
-from tensorflow.keras.applications.xception import (Xception,
-                                                    decode_predictions,
-                                                    preprocess_input)
+from tensorflow.keras.applications import resnet_v2, vgg16, xception
 
-input_shape = (299, 299)
+# module, model, input shape
+models = {"xception": (xception, xception.Xception(), (299, 299)),
+          "vgg16": (vgg16, vgg16.VGG16(), (224, 224)),
+          "resnet50": (resnet_v2, resnet_v2.ResNet50V2(), (224, 224))}
+
 
 def get_image(link):
     """
@@ -23,37 +24,46 @@ def get_image(link):
     return np.array(Image.open(BytesIO(r.content)))
 
 
-
-def make_plotly_plot(pred):
+def make_plotly_plot(pred, model_name):
+    """
+    Make the plotly plot given the predictions and model name.
+    """
     _, y, x = zip(*pred[::-1])
 
     trace1 = go.Bar(x=x, y=y, orientation='h', text=x,
                     textfont=dict(size=20), textposition='auto')
-    layout = go.Layout(title="Predictions", font=dict(size=24))
+    layout = go.Layout(title="Predictions from " + model_name, font=dict(size=24))
     fig = go.Figure(data=[trace1], layout=layout)
     return fig
 
-def classification_plot(img, model="xception", top=5):
+
+def classification_plot(img, model_name, top=5):
     """
     Return a plotly plot of decided predictions
 
     Assume img is Pillow object
     """
+    module, model, input_shape = models[model_name]
+
+    # Preprocess image
     img = np.array(img)
     img = cv2.resize(img, input_shape)
+    img = module.preprocess_input(img)
 
-    img = preprocess_input(img)
-    model = Xception()
+    # Get and decode predictions
     pred = model.predict(np.expand_dims(img, 0))
-    pred = decode_predictions(pred, top=top)[0]
+    pred = module.decode_predictions(pred, top=top)[0]
 
-    fig =  make_plotly_plot(pred)
+    fig = make_plotly_plot(pred, model_name)
     return fig
-
 
 
 if __name__ == "__main__":
     img = get_image("https://upload.wikimedia.org/wikipedia/commons/6/66/"
                     "An_up-close_picture_of_a_curious_male_domestic_shorthair_tabby_cat.jpg")
-    fig = classification_plot(img, top=10)
+    fig = classification_plot(img, model_name="resnet50")
+    fig.show()
+    fig = classification_plot(img, model_name="vgg16")
+    fig.show()
+    fig = classification_plot(img, model_name="xception")
     fig.show()
